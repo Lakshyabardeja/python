e6e2b20fa8c1078b9761240c91946118683c19e1
Lakshybardeja <lakshyabrdeja0000@gmail.com>
2024-05-17 17:27:00 +0530

    10th class report card

---
+++ report_card.py
@@ -0,0 +1,39 @@
+# number = int(input("type your specific number"))
+# if  number % 2 == 0:
+#   print("this is an even number")
+# else :
+#   print("this is an odd number")
+
+
+# age= int(input("whats ur age?"))
+# if age == +18 :
+#   print("Your ille")
+  
+
+# 0 - 333 fail 
+# 33 -50 grade d 
+# 50 - 70 gradfe c
+# 70 - 80 b
+# 80 - 90 a 
+#  90 - 100 a+
+# great 
+
+input("welcome to daisy international school,what is your name?")
+report_card= int(input("kitne nunber aaye beta aapke?"))
+if report_card <= 33 :
+  print("chullu bhar  paani mai doob jaa")
+elif  report_card <= 50 :
+  print("or krlo awarageerdi")
+elif report_card <= 70 :
+  print("agli baar acche se karna ")
+elif report_card <= 80 :
+  print("sabash betaaa")
+elif report_card <= 90 :
+  print("je chora to ips bnega")
+elif report_card <= 100 :
+  print("je chora to ias bnega")
+else :
+  print("sahi nunber to daalo bhai")
+  
+
+  
